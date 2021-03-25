f9f1de44221545763c041a4bb138b42f2c3ce89d
DMhlchnko <dan.2000.uami@gmail.com>
2021-03-25 13:01:46 +0200

    added validation annitations to DTO model

--- Models/DTO/RelationDTO.cs
+++ Models/DTO/RelationDTO.cs
@@ -1,18 +1,37 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AndersenCoreApp.Models.DTO
 {
     public class RelationDTO
     {
         public Guid Id { get; set; }
+
+        [Required,DataType(DataType.Text)]
         public string Name { get; set; }
+
+        [Required, DataType(DataType.Text)]
         public string FullName { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber)]
         public string TelephoneNumber { get; set; }
+
+        [Required, DataType(DataType.EmailAddress)]
         public string EMail { get; set; }
+
+        [Required, DataType(DataType.Text)]
         public string Street { get; set; }
+
+        [Required, DataType(DataType.Text)]
         public string City { get; set; }
+
+        [Required, DataType(DataType.Text)]
         public string Country { get; set; }
+
+        [Required, DataType(DataType.Text)]
         public string PostalCode { get; set; }
+
+        [Required]
         public int StreetNumber { get; set; }
     }
 }
